4ba06269bb351236c5aa515526b1f3565d1ab8cb
Usama Tahir <usamatahir00004@gmail.com>
2024-11-18 21:09:43 +0100

    ADDING BASIC STRUCTURE

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-url =  ""
+url =  "https://usama00004.github.io/portfolio/"
 
 r = requests.get(url)
+
+soup = BeautifulSoup(r.text, "lxml")
+print(soup.div)
