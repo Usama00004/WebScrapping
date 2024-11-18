510722dc4a6ddb06f4a81a84c042a94b6135caba
Usama Tahir <usamatahir00004@gmail.com>
2024-11-18 21:57:29 +0100

    Regular Expressions

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
 url =  "https://usama00004.github.io/portfolio/"
 
 r = requests.get(url)
 
 soup = BeautifulSoup(r.text, "lxml")
+
 print(soup.div)
+
+# Regular Expressions
+# data = soup.find_all(string= re.compile("string_to_find"))
+
